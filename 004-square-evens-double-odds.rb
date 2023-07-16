75c2ab6271be4c49db7ee4dc9673d4371b303ebe
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2023-07-16 17:58:03 -0600

    Update 004-square-evens-double-odds.rb

--- 004-square-evens-double-odds.rb
+++ 004-square-evens-double-odds.rb
@@ -1,7 +1,11 @@
+# Gets numerical input from user.
 res = gets.to_i
 
+# Checks if response is odd or even.
 if res % 2 == 0
+  # Squares evens.
   puts res**2
 else
+  # Doubles odds.
   puts res * 2
 end
